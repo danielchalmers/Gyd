bdbaf29796cff8f75706ac9bbdaeff0ddbc4bdc0
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-10-19 19:42:10 -0400

    Set up initial interface

---
+++ Gyd/MarkupExtensions/SystemTypeExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Gyd.MarkupExtensions
+{
+    public class SystemTypeExtension : MarkupExtension
+    {
+        private object _value;
+
+        public bool Bool
+        {
+            set => _value = value;
+        }
+
+        public double Double
+        {
+            set => _value = value;
+        }
+
+        public int Int
+        {
+            set => _value = value;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _value;
+        }
+    }
+}
