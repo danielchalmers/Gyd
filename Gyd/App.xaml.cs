8e09af5f7b02af27c39e24b322235302a02b8bee
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-10-19 11:18:42 -0400

    Add assembly info properties to App

--- Gyd/App.xaml.cs
+++ Gyd/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
+using Gyd.Utils;
 
 namespace Gyd
 {
@@ -13,5 +10,13 @@ namespace Gyd
     /// </summary>
     public partial class App : Application
     {
+        public static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+        public static string Company { get; } = Assembly.GetCompany();
+        public static string Copyright { get; } = Assembly.GetCopyright();
+        public static string Description { get; } = Assembly.GetDescription();
+        public static string Directory { get; } = Assembly.GetDirectory();
+        public static string Path { get; } = Assembly.GetPath();
+        public static string Title { get; } = Assembly.GetTitle();
+        public static Version Version { get; } = Assembly.GetVersion();
     }
 }
