38bee8928bcd9cdf3bca73576b89b1c217c0f2c4
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-10-20 15:38:19 -0400

    Hook up dialog to view model

--- Gyd/ViewModels/MainViewModel.cs
+++ Gyd/ViewModels/MainViewModel.cs
@@ -1,8 +1,47 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Gyd.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        private string _dialogText;
+        private bool _isDialogOpen;
+
+        public MainViewModel()
+        {
+            CloseDialogCommand = new RelayCommand<bool>(CloseDialogExecute);
+        }
+
+        public ICommand CloseDialogCommand { get; }
+
+        public string DialogText
+        {
+            get => _dialogText;
+            set => Set(ref _dialogText, value);
+        }
+
+        public bool IsDialogOpen
+        {
+            get => _isDialogOpen;
+            set
+            {
+                Set(ref _isDialogOpen, value);
+                if (!value)
+                {
+                    OnNewVideoDialogClosed();
+                }
+            }
+        }
+
+        private void CloseDialogExecute(bool result)
+        {
+            IsDialogOpen = false;
+        }
+
+        private void OnNewVideoDialogClosed()
+        {
+        }
     }
 }
