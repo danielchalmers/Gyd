96ba5ae38de3eae183b138320c89118c47b47324
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-12-10 13:27:42 -0500

    Revert "Persist selected format index"
    This reverts commit 66b348a82e19f307e3db26ba98f389e7e88a6594.

--- Gyd/Properties/Settings.Designer.cs
+++ Gyd/Properties/Settings.Designer.cs
@@ -37,18 +37,6 @@ namespace Gyd.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
-        public int SelectedFormatIndex {
-            get {
-                return ((int)(this["SelectedFormatIndex"]));
-            }
-            set {
-                this["SelectedFormatIndex"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
 <WindowPlacement xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
   <Length>0</Length>
